078ef2a53302b91c713cf47e8fea2fc70b2d0758
scopeinfinity <gagan1kumar@ymail.com>
2017-03-30 14:49:01 +0530

    Code

--- code.c
+++ code.c
@@ -1,2 +1,25 @@
-( )
-EOC
+const int global = 5;
+
+int abcd();
+
+int factorial(int x) {
+	if(x==0)
+		return 1;
+	return x*factorial(x-1);
+}
+
+void swap(int *x,int *y) {
+	int z=*y; int c;
+	*y=*x;
+	*x=z;
+}
+
+int main(){
+	int i;
+	for (i = 0; i < 100; ++i)
+	{
+		int z = factorial(i) % 20;
+		swap(z,i);
+		printf("%d\n",i );
+	}
+}
